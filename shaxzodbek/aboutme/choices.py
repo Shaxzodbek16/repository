4a89b17bc6de691423f4256e70904ae44b42b0a6
Shaxzodbek <134439368+Shaxzodbek16@users.noreply.github.com>
2024-06-11 15:32:09 +0500

    decide to make API

--- shaxzodbek/aboutme/choices.py
+++
@@ -1,9 +0,0 @@
-catagory_options: list[tuple] = [
-    ("all", "all"),
-    ("aboutme", "aboutme"),
-    ("love.html", "love.html"),
-    ("friends", "friends"),
-    ("family", 'family'),
-    ("projects.html", "projects.html"),
-    ("wish", "wish")
-]
--- shaxzodbek/shaxzodbek/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('__admin/', admin.site.urls),
-    path('', include('aboutme.urls')),
-]
