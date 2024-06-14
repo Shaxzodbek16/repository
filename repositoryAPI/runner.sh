d11bc5310500f72c4dce44b61f867ed333451d50
Shaxzodbek <134439368+Shaxzodbek16@users.noreply.github.com>
2024-06-15 03:08:05 +0500

    Create runner.sh

---
+++ repositoryAPI/runner.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Starting Django project setup..."
+sleep 4
+
+echo -n "Creating migrations..."
+python manage.py makemigrations
+echo "Done!"
+
+sleep 4
+
+echo -n "Applying migrations..."
+python manage.py migrate
+echo "Done!"
+
+sleep 4
+
+echo "Starting development server..."
+python manage.py runserver
