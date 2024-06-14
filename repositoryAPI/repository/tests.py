e2e612c54120582324b5bdad3fae860252c78759
Shaxzodbek <134439368+Shaxzodbek16@users.noreply.github.com>
2024-06-15 03:08:27 +0500

    write test for YouTube model and serializer

--- repositoryAPI/repository/tests.py
+++ repositoryAPI/repository/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import Person, Wish, Shaxzodbek, YouTube
+from .serializers import YouTubeSerializer
 
-# Create your tests here.
+
+class TestYoTube(TestCase):
+    def setUp(self) -> None:
+        self.client = Client()
+        self.youtube1 = YouTube.objects.create(title="YouTube title 1", description="YouTube description 1", link="https://www.youtube.com/link1")
+        self.youtube2 = YouTube.objects.create(title="YouTube title 2", description="YouTube description 2", link="https://www.youtube.com/link2")
+        self.youtube1 = YouTubeSerializer(self.youtube1)
+        self.youtube2 = YouTubeSerializer(self.youtube2)
+
+    def test_get_youtube(self):
+        response = self.client.get("/api/youtube/")
+        print(response.data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 2)
+        self.assertEqual(response.data[0], self.youtube1.data)
+        self.assertEqual(response.data[1], self.youtube2.data)
+
+    def test_post_youtube(self):
+        response = self.client.post("/api/youtube/")
+        self.assertEqual(response.status_code, 400)
+
+    def test_put_youtube(self):
+        response = self.client.put("/api/youtube/")
+        self.assertEqual(response.status_code, 404)
+
+    def test_delete_youtube(self):
+        response = self.client.delete("/api/youtube/1")
+        self.assertEqual(response.status_code, 204)
+        self.assertEqual(len(response.data), 1)
