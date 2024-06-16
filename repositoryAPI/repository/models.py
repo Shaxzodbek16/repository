611eddf53f7e17e3c69fece7618a2d7d2e23a882
Shaxzodbek <134439368+Shaxzodbek16@users.noreply.github.com>
2024-06-17 01:06:53 +0500

    game modeli yaratildi va u person bilan manytomany qilindi

--- repositoryAPI/repository/models.py
+++ repositoryAPI/repository/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 
 
+class Game(models.Model):
+	name = models.CharField(max_length=100)
+	description = models.TextField()
+	photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+	created_at = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.name
+
+
 class Person(models.Model):
 	first_name = models.CharField(max_length=50)
 	last_name = models.CharField(max_length=50)
@@ -11,6 +21,7 @@ class Person(models.Model):
 	field = models.CharField(max_length=50, null=True, blank=True)
 	where_connected = models.CharField(max_length=50, null=True, blank=True)
 	created_at = models.DateTimeField(auto_now_add=True)
+	game = models.ManyToManyField(Game)
 	views = models.PositiveBigIntegerField(default=0)
 
 	def __str__(self):
