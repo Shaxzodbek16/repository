f06149a7814818bd1901fec3d6cfcb17c240dd7c
Shaxzodbek <134439368+Shaxzodbek16@users.noreply.github.com>
2024-06-27 01:34:20 +0500

    authentication imkoniyati

--- repositoryAPI/repositoryAPI/settings.py
+++ repositoryAPI/repositoryAPI/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 
+from rest_framework import authentication
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -40,6 +42,7 @@ INSTALLED_APPS = [
 
     # added apps
     'repository',
+    'authentication',
 
     # three part apps
     'drf_yasg',
--- repositoryAPI/repositoryAPI/urls.py
+++ repositoryAPI/repositoryAPI/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path('', index, name='index'),
     path('admin/', admin.site.urls),
     path('api/', include('repository.urls')),
+    path('token/', include('authentication.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api-auth/', include('rest_framework.urls')),
