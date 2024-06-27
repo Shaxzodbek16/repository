c4d056f6daaaf78a118c14c994a3d4f1431c1052
Shaxzodbek <134439368+Shaxzodbek16@users.noreply.github.com>
2024-06-28 03:38:37 +0500

    Update settings.py

--- repositoryAPI/repositoryAPI/settings.py
+++ repositoryAPI/repositoryAPI/settings.py
@@ -133,7 +133,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = "/var/www/static/"
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
