26e5ff611adf562030be50cfd9881d7ea4e8aa76
Shaxzodbek <134439368+Shaxzodbek16@users.noreply.github.com>
2024-06-28 02:14:19 +0500

    Update runner.sh

--- repositoryAPI/runner.sh
+++ repositoryAPI/runner.sh
@@ -8,14 +8,9 @@ python manage.py makemigrations
 echo "Done!"
 
 sleep 4
-export DJANGO_SETTINGS_MODULE=repositoryAPI.settings  # Replace 'your_project' with your project's name
 
 echo -n "Applying migrations..."
 python manage.py migrate
-echo "Done!"
-sleep 4
-echo "Testing..."
-pytest
 sleep 4
 
 echo "Starting development server..."
