52e969c11e7a7791deea46615b3fb8a5c8bc0685
Shaxzodbek <134439368+Shaxzodbek16@users.noreply.github.com>
2024-06-28 04:18:12 +0500

    Update settings.py

--- repositoryAPI/repositoryAPI/settings.py
+++ repositoryAPI/repositoryAPI/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tor&*n@k+kxi8dcrc393rq+w00u4*f%5zflwm$2g924!1hb*6v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['api.shaxzodbek.com']
 
